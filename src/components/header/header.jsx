7c16de1cfde9c653554a559af7f57992a2960ab9
Siddhesh Jondhale <84581657+Siddhesh172004@users.noreply.github.com>
2022-10-23 16:12:04 +0530

    Update header.jsx

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <HeaderWrapper>
       <h1>BetterColor</h1>
-      <p>A Website For Finding Better Color Combinations For Readability</p>
+      <p class="sub-heading_text">A Website For Finding Better Color Combinations For Readability</p>
     </HeaderWrapper>
   );
 }
